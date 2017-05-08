79ed71d17c0ede782c79490df8eef93edb05d901
Cao Xuan Phong <caoxuanphong@Caos-MacBook-Pro.local>
2017-05-08 11:45:58 +0700

    First commit

---
+++ slack_delete.py
@@ -0,0 +1,36 @@
+import requests
+import time
+import json
+import sys
+
+token = param_1= sys.argv[1] 
+
+#Delete files older than this:
+ts_to = int(time.time()) - 30 * 24 * 60 * 60
+
+def list_files():
+  params = {
+    'token': token
+    ,'ts_to': ts_to
+    ,'count': 1000
+  }
+  uri = 'https://slack.com/api/files.list'
+  response = requests.get(uri, params=params)
+  return json.loads(response.text)['files']
+
+def delete_files(file_ids):
+  count = 0
+  num_files = len(file_ids)
+  for file_id in file_ids:
+    count = count + 1
+    params = {
+      'token': token
+      ,'file': file_id
+      }
+    uri = 'https://slack.com/api/files.delete'
+    response = requests.get(uri, params=params)
+    print count, "of", num_files, "-", file_id, json.loads(response.text)['ok']
+
+files = list_files()
+file_ids = [f['id'] for f in files]
+delete_files(file_ids)
